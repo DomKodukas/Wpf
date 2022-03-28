7229364c638b38a44c0b920d7173ff20dc1c7f1a
DomKodukas <dominykas.pugacius@stud.svako.lt>
2022-03-28 15:24:22 +0300

    Add project files.

---
+++ wpf/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string hash = "f0xle@rn";
+        private void btnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] data = UTF8Encoding.UTF8.GetBytes(txtValue.Text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode=CipherMode.ECB, Padding=PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripDes.CreateEncryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    txtEncrypt.Text = Convert.ToBase64String(results, 0, results.Length);
+                }
+            }       
+        }
+
+        private void btnDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] data = Convert.FromBase64String(txtEncrypt.Text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripDes.CreateDecryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    txtDecrypt.Text = UTF8Encoding.UTF8.GetString(results);
+                }
+            }
+        }
+    }
+}
